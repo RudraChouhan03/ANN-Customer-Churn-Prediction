b8eae870deb12752cd2563347c9a706c77ac1b89
Rudra Chouhan <rudrachouhan0305@gmail.com>
2025-06-26 15:56:00 +0530

    changed app.py

--- app.py
+++ app.py
@@ -34,7 +34,6 @@ age = st.slider('Age', 18, 92)
 # Get the balance from the user
 balance = st.number_input('Balance')
 credit_score = st.number_input('Credit Score')
-estimated_salary = st.number_input('Estimated Salary')
 tenure = st.slider('Tenure', 0, 10)
 num_of_products = st.slider('Number of Products', 1, 4)
 has_cr_card = st.selectbox('Has Credit Card', [0, 1])
@@ -50,7 +49,6 @@ input_data = pd.DataFrame({
     'NumOfProducts': [num_of_products],
     'HasCrCard': [has_cr_card],
     'IsActiveMember': [is_active_member],
-    'EstimatedSalary': [estimated_salary],
     'Exited': [0]
 })
 
